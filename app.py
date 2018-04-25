cf7a4ad8c6d47368aa7898ad6c75a0f086c32dfd
quyctd <skyvp98@gmail.com>
2018-04-25 16:50:37 +0700

    .

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 @app.route('/')
 
 def index():
-    gmail = GMail(username="duchoapc99techkids@gmail.com",
-                  password="duchoa119")
+    gmail = GMail("duchoapc99techkids@gmail.com",
+                  "duchoa119")
 
-    msg = Message("send mail", to="duchoapc99@gmail.com", text="Test")
+    msg = Message("send mail", to="quy.dc98@gmail.com", text="Test")
     gmail.send(msg)
 
     return "Sent"
